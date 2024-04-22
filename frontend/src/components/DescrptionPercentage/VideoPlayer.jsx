0c858397b5410ebc36e8eb9917b61dcbcbfae09d
NAlonsoBulacio <nicoalonso99.na@gmail.com>
2024-04-22 07:33:31 -0300

    e prevent default

--- frontend/src/components/DescrptionPercentage/VideoPlayer.jsx
+++ frontend/src/components/DescrptionPercentage/VideoPlayer.jsx
@@ -1,10 +1,13 @@
 import React, { useRef, useState } from "react";
 import { logo_blanco } from "../../assets";
+import { FaRegCirclePlay } from "react-icons/fa6";
+
 function VideoPlayer({ url }) {
   const videoRef = useRef(null);
   const [isPlaying, setIsPlaying] = useState(false);
 
-  const handleClick = () => {
+  const handleClick = (event) => {
+    event.preventDefault();
     if (isPlaying) {
       videoRef.current.pause();
     } else {
@@ -13,12 +16,15 @@ function VideoPlayer({ url }) {
     setIsPlaying(!isPlaying); 
   };
   return (
-    <div className="w-[100%] ">
+    <div className="w-[100%]">
       <div className=" rounded-xl relative">
       <video ref={videoRef} muted onClick={handleClick}>
           <source src={url} type="video/mp4" />
           Tu navegador no soporta el elemento de video.
         </video>
+        <div className="absolute top-[50%] left-[50%]">
+          <FaRegCirclePlay className="text-5xl" />
+        </div>
         <div className="absolute bottom-2 left-4 flex items-center space-x-2">
           <div className="rounded-full bg-white w-[30px] h-[30px] flex justify-center items-center overflow-hidden">
             <img src={logo_blanco} alt="" className="w-6" />
