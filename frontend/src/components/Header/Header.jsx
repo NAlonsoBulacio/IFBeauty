3ddb0302d0e350e3cc54d25b673e2b82ee93989e
NAlonsoBulacio <nicoalonso99.na@gmail.com>
2024-05-02 15:19:05 -0300

    header mobile fixde

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -8,10 +8,10 @@ const Header = () => {
   return (
     <div>
       <div
-        className="z-40 top-0 right-0 w-screen bg-slate-600 mx-auto flex items-center px-10 py-6 justify-between lg:hidden"
+        className="z-40 top-0 right-0 w-screen bg-slate-600 mx-auto flex items-center py-6 justify-center lg:hidden"
         style={{ maxWidth: "1150px", height: "100px" }}
       >
-        <div className="w-1/2 flex items-center ">
+        <div className="flex items-center ">
           <a href="/">
             <img className="w-[105px] " src={logo} alt="Logo" />
           </a>
