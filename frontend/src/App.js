638baea0ef571d3daa23c82e14bafd3ee74b7930
NAlonsoBulacio <nicoalonso99.na@gmail.com>
2024-05-06 19:49:37 -0300

    landing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import { Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage/LandingPage";
 import Test from './pages/LandingPage/Test/Test';
+import LandingPageProduct from './pages/LandingPageProduct/LandingPageProducts';
 function App() {
 
 
   return (
     <div className="App font-sans-200">
       <Route exact path="/" component={LandingPage} />
+      <Route exact path="/collagen" component={LandingPageProduct} />
       <Route exact path="/test" component={Test} />
     </div>
   );
--- frontend/src/components/Header/HeaderProduct.jsx
+++ frontend/src/components/Header/HeaderProduct.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { logo } from "../../assets/index";
-const Header = ({}) => {
+const HeaderProduct = ({}) => {
   const [menuOpen, setMenuOpen] = useState(false);
   const handleMenuToggle = () => {
     setMenuOpen(!menuOpen);
@@ -8,7 +8,7 @@ const Header = ({}) => {
   return (
     <div>
       <div
-        className={`z-40 top-0 right-0 w-screen bg-slate-600 mx-auto flex items-center py-6 justify-center lg:hidden`}
+        className={`z-40 top-0 right-0 w-screen bg-white mx-auto flex items-center py-6 justify-center lg:hidden`}
         style={{ maxWidth: "1150px", height: "100px" }}
       >
         <div className="flex items-center ">
@@ -17,7 +17,7 @@ const Header = ({}) => {
           </a>
         </div>
       </div>
-      <div className="hidden lg:flex justify-center items-center  w-screen h-32 bg-slate-600 shadow-lg lg:px-20 xl:px-36 py-2 z-40">
+      <div className="hidden lg:flex justify-center items-center  w-screen h-32  shadow-lg lg:px-20 xl:px-36 py-2 z-40">
         <div className="flex justify-center items-center h-full">
           <img className="h-full" src={logo} alt="logo-galindosa" />
         </div>
@@ -27,4 +27,4 @@ const Header = ({}) => {
   );
 };
 
-export default Header;
+export default HeaderProduct;
