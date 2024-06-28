331765dd85c59d623ea497dba048cb7d36ad07da
NAlonsoBulacio <nicoalonso99.na@gmail.com>
2024-06-28 15:24:40 -0300

    useEffect fixed

--- frontend/src/components/Reviews/Reviews.jsx
+++ frontend/src/components/Reviews/Reviews.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Stars from "./Stars";
 import star from "../../assets/star.svg";
 import { reviews, acne_reviews } from "./AllReviews";
